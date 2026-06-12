bf10ac653b5dc563c36d6922911c17fd33633422
HendrDirga <hendradirga06@gmail.com>
2026-06-12 12:28:05 +0700

    Update User.java

--- src/matcha/model/User.java
+++ src/matcha/model/User.java
@@ -5,29 +5,76 @@
 package matcha.model;
 
 /**
- *
  * Modul 1: Manajemen Pengguna
  */
 public abstract class User {
+
     protected String id;
     protected String nama;
     protected String email;
     protected String password;
     protected String noTelp;
 
+    public User() {
+    }
+
+    public User(String id, String nama, String email,
+                String password, String noTelp) {
+        this.id = id;
+        this.nama = nama;
+        this.email = email;
+        this.password = password;
+        this.noTelp = noTelp;
+    }
+
     public boolean login(String email, String pass) {
-        // TODO: Buat logika validasi email dan password
-        return false;
+        return this.email.equals(email)
+                && this.password.equals(pass);
     }
 
     public void logout() {
-        // TODO: Buat logika penghapusan sesi
+        System.out.println(nama + " berhasil logout.");
     }
 
-    // Method abstrak yang wajib di-override oleh kelas turunannya
     public abstract boolean verifyIdentity();
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getNama() {
-        return this.nama;
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
     }
 }
