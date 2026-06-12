91933d5556cac2f19c283ef98c56574ab579b5d8
HendrDirga <hendradirga06@gmail.com>
2026-06-12 12:29:36 +0700

    Update Talent.java

--- src/matcha/model/Talent.java
+++ src/matcha/model/Talent.java
@@ -4,28 +4,55 @@
  */
 package matcha.model;
 
-/**
- *
- * Modul 1: Manajemen Pengguna
- */
-import java.util.ArrayList;
-
 public class Talent extends User {
-    private Profile profile;
-    private ArrayList<Schedule> scheduleList = new ArrayList<>();
-    private boolean isAvailable;
+
+    private String kategori;
+    private double tarifPerJam;
+    private boolean tersedia;
+
+    public Talent() {
+    }
+
+    public Talent(String id, String nama, String email,
+                  String password, String noTelp,
+                  String kategori, double tarifPerJam,
+                  boolean tersedia) {
+
+        super(id, nama, email, password, noTelp);
+        this.kategori = kategori;
+        this.tarifPerJam = tarifPerJam;
+        this.tersedia = tersedia;
+    }
 
     @Override
     public boolean verifyIdentity() {
-        // TODO: Implementasi verifikasi KTP/portofolio khusus Talent
-        return false;
+        return email != null
+                && !email.isEmpty()
+                && noTelp != null
+                && !noTelp.isEmpty();
     }
 
-    public void toggleAvailability() {
-        // TODO: Ubah status isAvailable (true ke false, atau sebaliknya)
+    public String getKategori() {
+        return kategori;
     }
 
-    public void addSchedule(Schedule jadwal) {
-        // TODO: Tambahkan jadwal baru ke scheduleList
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+
+    public double getTarifPerJam() {
+        return tarifPerJam;
+    }
+
+    public void setTarifPerJam(double tarifPerJam) {
+        this.tarifPerJam = tarifPerJam;
+    }
+
+    public boolean isTersedia() {
+        return tersedia;
+    }
+
+    public void setTersedia(boolean tersedia) {
+        this.tersedia = tersedia;
     }
 }
