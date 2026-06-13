4d4cbe2f3d3cf65c8f06e5c1060aebc65fac5812
HendrDirga <hendradirga06@gmail.com>
2026-06-13 12:18:55 +0700

    Update User.java

--- src/matcha/model/User.java
+++ src/matcha/model/User.java
@@ -4,9 +4,6 @@
  */
 package matcha.model;
 
-/**
- * Modul 1: Manajemen Pengguna
- */
 public abstract class User {
 
     protected String id;
@@ -15,11 +12,12 @@ public abstract class User {
     protected String password;
     protected String noTelp;
 
-    public User() {
-    }
+    public User(String id,
+                String nama,
+                String email,
+                String password,
+                String noTelp) {
 
-    public User(String id, String nama, String email,
-                String password, String noTelp) {
         this.id = id;
         this.nama = nama;
         this.email = email;
@@ -27,13 +25,17 @@ public abstract class User {
         this.noTelp = noTelp;
     }
 
-    public boolean login(String email, String pass) {
+    public boolean login(String email,
+                         String pass) {
+
         return this.email.equals(email)
                 && this.password.equals(pass);
     }
 
     public void logout() {
-        System.out.println(nama + " berhasil logout.");
+
+        System.out.println(
+                nama + " berhasil logout.");
     }
 
     public abstract boolean verifyIdentity();
@@ -42,24 +44,16 @@ public abstract class User {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getNama() {
         return nama;
-    }
-
-    public void setNama(String nama) {
-        this.nama = nama;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public String getNoTelp() {
+        return noTelp;
     }
 
     public String getPassword() {
@@ -68,13 +62,5 @@ public abstract class User {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getNoTelp() {
-        return noTelp;
-    }
-
-    public void setNoTelp(String noTelp) {
-        this.noTelp = noTelp;
     }
 }
